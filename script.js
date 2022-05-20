0de4192612c5b6fa7dcd09930b44de270da21351
Calvin H <105633795+calvinh10@users.noreply.github.com>
2022-05-20 17:05:51 +0100

    Create script.js
    JS file

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
+let count = 0
+
+
+function increment () {
+    count +=1
+    countEl.innerText = count
+}
+
+function decrease () {
+    count -=1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.innerHTML += countStr
+
+    countEl.innerText=0
+    count=0
+
+    
+}
+
